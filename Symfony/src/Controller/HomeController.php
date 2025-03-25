20bc95532c8c89f26900942a803b0224ed0cea02
simon <simon2002huang@gmail.com>
2025-03-25 16:05:49 +0100

    PanneauDeControle pour Administrateur

--- Symfony/src/Controller/HomeController.php
+++ Symfony/src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(UserRepository $repo): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
--- Symfony/src/DataFixtures/AppFixtures.php
+++ Symfony/src/DataFixtures/AppFixtures.php
@@ -4,13 +4,18 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use App\Entity\ConnectedObject;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new User;
+        $product->setUsers('visiteur');
+        $manager->persist($product);
+
+        
 
         $manager->flush();
     }
